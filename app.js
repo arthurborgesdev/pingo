851b41e4a27ffd0d85fda254bfa973abdce763e0
Arthur Borges <arthurborges.dev@gmail.com>
2018-11-28 10:23:51 -0200

    Finalizei as telas de login e de registro, com servidor redirecionando e coletando os inputs de registro

--- app.js
+++ app.js
@@ -5,17 +5,27 @@ const express = require('express'),
       path = require('path')
       bodyParser = require('body-parser')
 
+app.use(bodyParser.urlencoded({extended: false}))
+app.use(bodyParser.json())
+
 app.use(express.static(path.join(__dirname, 'public')))
 app.use('/public', express.static(path.join(__dirname, 'public')));
 
 app.set('views', './views')
 app.set('view engine', 'pug')
 
-app.use(bodyParser.urlencoded({extended: false}))
-app.use(bodyParser.json())
-
 app.get('/', (req, res) => 
 	res.render('index'))
+
+app.get('/register', (req, res) => 
+	res.render('register'))
+
+app.post('/register', (req, res) => {
+	console.log(req.body)
+	// aqui fica a chamada pros controllers
+	res.redirect('/')
+})
+ 
 
 app.listen(port, () =>
 	console.log(`Server started on port ${port}`));
