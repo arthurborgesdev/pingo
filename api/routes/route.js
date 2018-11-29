e0c6f680fd5fa5eb0bef14486546000533996d68
Arthur Borges <arthurborges.dev@gmail.com>
2018-11-29 09:06:07 -0200

    Requesting ip of client

--- api/routes/route.js
+++ api/routes/route.js
@@ -9,7 +9,8 @@ module.exports = function(app) {
 		  res.render('register'))
 
 	  .post((req, res) => {
-	  	console.log(req)
+	  	console.log(req.ip)
+	  	console.log("177.133.15.109")
 		  console.log(req.body)
 		  // aqui fica a chamada pros controllers
 		  res.redirect('/')
