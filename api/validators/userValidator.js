df4267b30e1657581fc7122a1bc362658ee01246
Arthur Borges <arthurborges.dev@gmail.com>
2018-11-30 09:21:26 -0200

    Fixed name must contain alpha num caracters

--- api/validators/userValidator.js
+++ api/validators/userValidator.js
@@ -3,7 +3,7 @@ const Joi = require('joi')
 exports.validateInput = function(req, res, next) {
    
   const inputSchema = Joi.object().keys({
-  	name: Joi.string().alphanum().min(3).max(30).required(),
+  	name: Joi.string().min(3).max(30).required(),
     email: Joi.string().email({ minDomainAtoms: 2 }),
   	password: Joi.string().regex(/^[a-zA-Z0-9]{3,30}$/),
   });
