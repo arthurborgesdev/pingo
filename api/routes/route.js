bd0506b2296a08a7efb0bd4bd48dbb40878c858a
Arthur Borges <arthurborges.dev@gmail.com>
2018-12-10 11:16:13 -0200

    Finalizei a parte de verificar se o usuário está logado ou não antes de deixá-lo adentrar no mapa

--- api/routes/route.js
+++ api/routes/route.js
@@ -1,39 +1,41 @@
 const userController = require('../controllers/userController'),
       passport = require('passport')
       const router = require('express').Router();
+      const loggedIn = require('../middlewares/loggedIn').loggedIn;
 
 module.exports = function(app) {
-	
-	app.route('/')
-	  .get(function(req, res) {
-		  res.render('index', {isCorrect: true})
-	  })
-		.post(function(req, res, next) {
-      passport.authenticate('local', function(err, user, info) {
-      	if(err) {return next(err);}
-      	//console.log("USER AQUI");
-      	//console.log(user);
-      	if (!user) { return res.render('index', {isCorrect: false })}
-      	//req.login(user, next);
-        res.redirect('/map')
 
-      })(req, res, next)
-	  })
+	app.get('/', function(req, res) {
+		res.render('index', {isCorrect: true})
+	})
 
-	app.route('/register')
-	  .get(function(req, res) {
-		  res.render('register', {isHuman: true})
-	  })
+	app.post('/', function(req, res, next) {
+    passport.authenticate('local', function(err, user, info) {
+      if(err) {return next(err);}
+      //console.log("USER AQUI");
+      console.log(user);
+      if (!user) { return res.render('index', {isCorrect: false })}
+      req.login(user, function(err) {
+        if (err) { return err; }
+      	// aqui vai ser necessário se a rota pegar o uuid do user.name (:uid)
+      	return res.redirect('map')
+      });
+
+    })(req, res, next)
+	})
+
+	app.get('/register', function(req, res) {
+		res.render('register', {isHuman: true})
+	})
 		  
-	  .post(function(req, res) {
-	  	userController.handleInput(req, res)
-	  })
-	  	
+	app.post(function(req, res) {
+	  userController.handleInput(req, res)
+	})
 
-  app.route('/map')
-    .get(function(req, res) {
-  	  res.render('map')
-    })
- 
-}
+	app.get('/map', loggedIn, function(req, res) {
+
+	  passport.authorize('local', { failureRedirect:'/' })
+		res.render('map')
+	})
+} 
 	
